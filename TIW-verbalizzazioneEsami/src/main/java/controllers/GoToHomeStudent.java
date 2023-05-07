ba2480b06b1c1cd7075ed479d7ecc435b7e16c6d
ari-got <arianna.gottardi@mail.polimi.it>
2023-05-07 22:10:08 +0200

    login

---
+++ TIW-verbalizzazioneEsami/src/main/java/controllers/GoToHomeStudent.java
@@ -0,0 +1,51 @@
+package controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+
+
+@WebServlet("/GoToHomeStudent")
+public class GoToHomeStudent extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private TemplateEngine templateEngine;
+
+	public GoToHomeStudent() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public void init() throws ServletException {
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String path = "/WEB-INF/StudentHomePage.html";
+		ServletContext servletContext = getServletContext();
+		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+		templateEngine.process(path, ctx, response.getWriter());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- TIW-verbalizzazioneEsami/src/main/java/dao/UserDAO.java
+++ TIW-verbalizzazioneEsami/src/main/java/dao/UserDAO.java
@@ -1,1 +1,53 @@
 //userDAO
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import beans.User;
+
+public class UserDAO {
+	private Connection con;
+
+	public UserDAO(Connection connection) {
+		this.con = connection;
+	}
+
+	public User checkCredentialsTeacher(String usrn, String pwd) throws SQLException {
+		String query = "SELECT  matricola FROM teacher  WHERE username = ? AND password =? ";
+		try (PreparedStatement pstatement = con.prepareStatement(query);) {
+			pstatement.setString(1, usrn);
+			pstatement.setString(2, pwd);
+			try (ResultSet result = pstatement.executeQuery();) {
+				if (!result.isBeforeFirst()) // no results, credential check failed
+					return null;
+				else {
+					result.next();
+					User user = new User();
+					user.setMatricola(result.getInt("id"));
+					user.setRole(result.getString("teacher"));
+					return user;
+				}
+			}
+		}
+	}
+	public User checkCredentialsStudent(String usrn, String pwd) throws SQLException {
+		String query = "SELECT  matricola FROM student  WHERE username = ? AND password =? ";
+		try (PreparedStatement pstatement = con.prepareStatement(query);) {
+			pstatement.setString(1, usrn);
+			pstatement.setString(2, pwd);
+			try (ResultSet result = pstatement.executeQuery();) {
+				if (!result.isBeforeFirst()) // no results, credential check failed
+					return null;
+				else {
+					result.next();
+					User user = new User();
+					user.setMatricola(result.getInt("id"));
+					user.setRole(result.getString("student"));
+					return user;
+				}
+			}
+		}
+	}
+}
