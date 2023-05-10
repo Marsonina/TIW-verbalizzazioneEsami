c81b398044497834f4691bef31069093700f8b7e
Silvia <silvia.ferrara@mail.polimi.it>
2023-05-10 15:51:30 +0200

    add ExamDAO

--- TIW-verbalizzazioneEsami/src/main/java/dao/ExamDAO.java
+++ TIW-verbalizzazioneEsami/src/main/java/dao/ExamDAO.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import beans.User;
+import beans.User; 
 
 public class ExamDAO {
 	private Connection connection;
