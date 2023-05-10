7c9c58341a418b3bbe8f0640c147124ae2d6b45d
ari-got <arianna.gottardi@mail.polimi.it>
2023-05-10 17:51:15 +0200

    Add Enrolled Student Page Version1

--- TIW-verbalizzazioneEsami/src/main/java/dao/ExamDAO.java
+++ TIW-verbalizzazioneEsami/src/main/java/dao/ExamDAO.java
@@ -4,8 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import beans.User; 
+import java.util.ArrayList;
+import java.util.List;
 
+import beans.User; 
+import beans.ExamStudent;
 public class ExamDAO {
 	private Connection connection;
 	private int courseId;
@@ -14,7 +17,36 @@ public class ExamDAO {
 	
 	public ExamDAO(Connection connection, int courseId, String chosenDate) {
 		this.connection = connection;
+		this.courseId = courseId;
 		this.chosenDate = chosenDate;
-		this.chosenDate = chosenDate;
+		
+	}
+	
+	public List<ExamStudent> getStudents() throws SQLException {
+		List<ExamStudent> users = new ArrayList<ExamStudent>();
+		String query = "SELECT student.matricola, student.name, student.surname, student.degree, student.email, exam_students.result, exam_students.resultState"
+				+ " FROM student, exam_students WHERE matricola = matricolaStudent AND courseId = 1 AND examDate = '2023-01-01'";
+		try (PreparedStatement pstatement = connection.prepareStatement(query);) {
+			//pstatement.setInt(1, courseId);
+			//pstatement.setString(2, chosenDate);
+			try (ResultSet result = pstatement.executeQuery();) {
+				if (!result.isBeforeFirst()) // no results, credential check failed
+					return null;
+				else {
+					while(result.next()) {
+						ExamStudent student = new ExamStudent();
+						student.setMatricola(result.getString("matricola"));
+						student.setName(result.getString("name"));
+						student.setSurname(result.getString("surname"));
+						student.setDegree(result.getString("degree"));
+						student.setEmail(result.getString("email"));
+						student.setResult(result.getString("result"));
+						student.setResultState(result.getString("resultState"));
+						users.add(student);
+					}
+				}
+			}
+		}
+		return users;
 	}
 }
