a2a543587295d782cc87a6da9758c914bbe22b11
ari-got <arianna.gottardi@mail.polimi.it>
2023-05-10 12:20:11 +0200

    Add GoToHomeTeacher and GoToHomeStudent

--- TIW-verbalizzazioneEsami/src/main/java/dao/CourseDAO.java
+++ TIW-verbalizzazioneEsami/src/main/java/dao/CourseDAO.java
@@ -5,6 +5,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import beans.User;
 import beans.Course;
 
@@ -17,8 +20,8 @@ public class CourseDAO {
 		this.id = i;
 	}
 
-/*	public Course findCourseTeacher() throws SQLException {
-		String query = "SELECT courseId, name FROM teacher, course  WHERE matricola = matricolaTeacher AND courseId = ? ";
+	public Course findCourse() throws SQLException {
+		String query = "SELECT id, name FROM course WHERE id = ? ";
 		try (PreparedStatement pstatement = con.prepareStatement(query);) {
 			pstatement.setInt(1, id);
 			try (ResultSet result = pstatement.executeQuery();) {
@@ -27,15 +30,15 @@ public class CourseDAO {
 				else {
 					result.next();
 					Course course = new Course();
-					course.setCourseId(result.getInt("courseId"));
-					course.setCourseName(result.getString("courseName"));
+					course.setCourseId(result.getInt("id"));
+					course.setCourseName(result.getString("name"));
 					return course;
 				}
 			}
 		}
 	}
-	public Course findCourseStudent() throws SQLException {
-		String query = "SELECT courseId, name FROM student, course_students WHERE mqatricola = matricolaStudent AND courseId = ? ";
+	public String findOwnerTeacher() throws SQLException {
+		String query = "SELECT matricolaTeacher FROM course WHERE id= ?";
 		try (PreparedStatement pstatement = con.prepareStatement(query);) {
 			pstatement.setInt(1, id);
 			try (ResultSet result = pstatement.executeQuery();) {
@@ -43,51 +46,28 @@ public class CourseDAO {
 					return null;
 				else {
 					result.next();
-					Course course = new Course();
-					course.setCourseId(result.getInt("courseId"));
-					course.setCourseName(result.getString("courseName"));
-					return course;
+					return result.getString("matricolaTeacher");
 				}
 			}
 		}
 	}
-	public User findOwnerTeacher() throws SQLException {
-		String query = "SELECT teacher.matricola, teacher.name, teacher.surname FROM teacher,course  WHERE matricola = matricolaTeacher courseId= ?";
-		try (PreparedStatement pstatement = con.prepareStatement(query);) {
-			pstatement.setInt(1, id);
-			try (ResultSet result = pstatement.executeQuery();) {
-				if (!result.isBeforeFirst()) // no results, credential check failed
-					return null;
-				else {
-					result.next();
-					User user = new User();
-					user.setMatricola(result.getString("matricola"));
-					user.setName(result.getString("name"));
-					user.setSurname("surname");
-					user.setRole("teacher");
-					return user;
-				}
-			}
-		}
-	}
+
 	
-	public User findAttendingStudent() throws SQLException {
-		String query = "SELECT student.matricola, student.name, student.surname FROM student,course_students,exam_s  WHERE matricola = matricolaTeacher courseId= ?";
+	public List<String> findAttendingStudent() throws SQLException {
+		List<String> users = new ArrayList<String>();
+		String query = "SELECT matricolaStudent FROM course_students WHERE courseId= ?";
 		try (PreparedStatement pstatement = con.prepareStatement(query);) {
 			pstatement.setInt(1, id);
 			try (ResultSet result = pstatement.executeQuery();) {
 				if (!result.isBeforeFirst()) // no results, credential check failed
 					return null;
 				else {
-					result.next();
-					User user = new User();
-					user.setMatricola(result.getString("matricola"));
-					user.setName(result.getString("name"));
-					user.setSurname("surname");
-					user.setRole("student");
-					return user;
+					while(result.next()) {
+						users.add(result.getString("matricolaStudent"));
+					}
 				}
 			}
 		}
-	}*/
+		return users;
+	}
 }
